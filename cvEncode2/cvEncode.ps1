fa97a5ddba8227e753ad891b2fc7a4018f5b4f20
Charlotte <hunandy14@gmail.com>
2021-10-05 01:14:52 +0900

    更新版本。不知道為什麼判定資料夾無效

--- cvEncode2/cvEncode.ps1
+++ cvEncode2/cvEncode.ps1
@@ -1,68 +1,74 @@
 [CmdletBinding()]
 Param(
-    $srcEnc,
-    $dstEnc,
-    $srcFile,
-    $dstFile
+    [String]$srcEnc,
+    [String]$dstEnc,
+    [String]$srcPath,
+    [String]$dstPath, 
+    [String]$Filter
 )
 # =========================
 class cvEncode {
-    [string]$srcEncode
-    [string]$dstEncode
+    [string]$srcEnc
+    [string]$dstEnc
     [System.Object]$Filter = @("*.*", "*.*")
     # Constructor
-    cvEncode($srcEncode, $dstEncode){
-        $this.srcEncode=$srcEncode
-        $this.dstEncode=$dstEncode
+    cvEncode($srcEnc, $dstEnc){
+        $this.srcEnc=$srcEnc
+        $this.dstEnc=$dstEnc
     }
-    cvEncode($srcEncode, $dstEncode, $Filter){
-        $this.srcEncode=$srcEncode
-        $this.dstEncode=$dstEncode
+    cvEncode($srcEnc, $dstEnc, $Filter){
+        $this.srcEnc=$srcEnc
+        $this.dstEnc=$dstEnc
         $this.Filter=$Filter
     }
     # フォルダのリストを獲得
-    [System.Object]getFoldList($srcFold, $dstFold) {
-        $fileItem =  Get-ChildItem -Path $srcFold.TrimEnd('\') `
-                        -Recurse -Include $this.Filter | Sort-Object
-        return $fileItem
+    [System.Object]getFoldList($srcPath) {
+        return (dir $srcPath.TrimEnd('\') -R -I $this.Filter)
     }
     # ファイルのエンコードの変換
-    [Void]convert($srcFile, $dstFile) {
-        New-Item -ItemType File -Path $dstFile -Force | Out-Null
-        $ct = Get-Content -Encoding $this.srcEncode $srcFile
-        $ct | Out-File -Encoding $this.dstEncode -FilePath $dstFile
+    [Void]convert($srcPath, $dstPath) {
+        New-Item -ItemType File -Path $dstPath -Force | Out-Null
+        $ct = Get-Content -Encoding $this.srcEnc $srcPath
+        $ct | Out-File -Encoding $this.dstEnc -FilePath $dstPath
     }
-    [Void]convert($srcFile) {
-        $dstFile = $srcFile.Substring($srcFile.LastIndexOf("\"))
-        $this.convert($srcFile, ("$PSScriptRoot\Output\"+$dstFile))
+    [Void]convert($srcPath) {
+        $dstPath = $srcPath.Substring($srcPath.LastIndexOf("\"))
+        $this.convert($srcPath, ("Output\"+$dstPath))
     }
     # フォルダのエンコードの変換
-    [Void]convertDir($srcFold, $dstFold){
-        $fileItem = $this.getFoldList($srcFold, $dstFold)
-        $srcFold = $srcFold.TrimEnd('\')
-        $dirName = $srcFold.Substring($srcFold.LastIndexof("\")+1)
-        Write-Warning ("Convert Files:: [" +$this.srcEncode+ " --> " +$this.dstEncode+ "]")
+    [Void]convertDir($srcPath, $dstPath){
+        $fileItem = $this.getFoldList($srcPath)
+        $srcPath = $srcPath.TrimEnd('\')
+        $dirName = $srcPath.Substring($srcPath.LastIndexof("\")+1)
+        Write-Warning ("Convert Files:: [" +$this.srcEnc+ " --> " +$this.dstEnc+ "]")
         for ($i = 0; $i -lt $fileItem.Count; $i++) {
             $F1=$fileItem[$i].FullName
-            $F2=$dstFold.TrimEnd('\')+"\"+$dirName+$F1.Substring($srcFold.Length)
+            $F2=$dstPath.TrimEnd('\')+"\"+$dirName+"\"+$fileItem[$i].Name
             Write-Warning "  From: $F1"
             Write-Warning "  └─To: $F2"
             $this.convert($F1, $F2)
         }
     }
-    [Void]convertDir($srcFold){
-        $this.convertDir($srcFold, ("$PSScriptRoot\Output\" + $this.dstEncode))
+    [Void]convertDir($srcPath){
+        $this.convertDir($srcPath, "Output")
     }
 }
 # ==============================================================================
 # プログラム開始
 # ==============================================================================
-if ([System.IO.Directory]::Exists($srcFile)) {
-    $cvEnc = [cvEncode]::new($srcEnc, $dstEnc, @("*.*"))
-    $cvEnc.convertDir($srcFile, $dstFile)
-}if ([system.IO.File]::Exists($srcFile)) {
-    $cvEnc = [cvEncode]::new($srcEnc, $dstEnc)
-    $cvEnc.convert($srcFile, $dstFile)
+# .\cvEncode.ps1 Shift-JIS utf8 dir\Shift-JIS.txt utf8.txt
+$srcEnc = 'Shift-JIS'
+$dstEnc = 'utf8'
+$srcPath = 'dir\Shift-JIS.txt'
+$srcPath = 'dir'
+$dstPath = 'utf8.txt'
+$dstPath = 'Output'
+if ([System.IO.Directory]::Exists($srcPath)) {
+    "dir Go"
+    [cvEncode]::new($srcEnc, $dstEnc, @("*.*")).convertDir($srcPath, $dstPath)
+}if ([system.IO.File]::Exists($srcPath)) {
+    "File Go"
+    [cvEncode]::new($srcEnc, $dstEnc).convert($srcPath, $dstPath)
 }
 # ==============================================================================
 
