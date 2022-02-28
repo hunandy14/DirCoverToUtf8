ec8fc4f59909f31c5ef488ade3ce29e20bc994ba
Charlotte.Hong <hunandy14@gmail.com>
2022-03-01 02:22:23 +0900

    1行或0行讀的時候都會被判定是1行，沒辦法搞定了。直接寫了

--- cvEncoding.ps1
+++ cvEncoding.ps1
@@ -1,15 +1,3 @@
-function cvEncode{
-    param (
-        $scrPath,
-        $dstPath
-    )
-    $Content = Get-Content $scrPath -Encoding:Default
-    $Encode = New-Object System.Text.UTF8Encoding $False
-    New-Item -ItemType File -Path $dstPath -Force | Out-Null
-    [System.IO.File]::WriteAllLines($dstPath, $Content, $Encode)
-}
-# cvEncodeToUTF8 Encode_Default.txt Encode_UTF8.txt
-
 function FileTrim {
     param (
         [System.Object] $Content,
@@ -17,11 +5,12 @@ function FileTrim {
     )
     for ($i = 0; $i -lt $Content.Count; $i++) {
         $Content[$i] = $Content[$i].TrimEnd(" |`t")
-        if($Content[$i] -ne ""){ $Line = $i }
+        if ($Content[$i] -ne "") { $Line = $i }
     } $Content = $Content[0..($Line)]
     return $Content
 }
 function ReadContent {
+    [CmdletBinding(DefaultParameterSetName = "A")]
     param (
         [Parameter(Mandatory, Position = 0, ParameterSetName = "")]
         [string] $Path,
@@ -37,62 +26,129 @@ function ReadContent {
         Write-Error "路徑不存在"
         return
     }
+    
     # 獲取編碼
-    if ((!$Encoding) ) {
+    if ($DefaultEncoding) {
+        # 使用當前系統編碼
+        $Enc = [Text.Encoding]::Default
+    }
+    elseif ((!$Encoding) ) {
+        # 完全不指定預設預設URF8
         $Enc = New-Object System.Text.UTF8Encoding $False
-        # $Enc = [Text.Encoding]::Default
-    } elseif ($Encoding -eq 65001) {
+    }
+    elseif ($Encoding -eq 65001) {
+        # 指定UTF8
         $Enc = New-Object System.Text.UTF8Encoding $False
-    } else {
+    }
+    else {
+        # 使用者指定
         $Enc = [Text.Encoding]::GetEncoding($Encoding)
-    } 
+    }
+    
     # 讀取檔案
-    [System.IO.File]::ReadAllLines($Path, $Enc)
+    $Content = [System.IO.File]::ReadAllLines($Path, $Enc)
     return $Content
-} # ReadContent "Encoding_SHIFT.txt" -Encoding:932
+}
+# ReadContent "Encoding_SHIFT.txt" 932
+# ReadContent "Encoding_UTF8.txt"
+
 function WriteContent {
+    [CmdletBinding(DefaultParameterSetName = "D")]
     param (
         [Parameter(Mandatory, Position = 0, ParameterSetName = "")]
         [string] $Path,
-        [Parameter(Position = 1, ParameterSetName = "A")]
-        [Parameter(Position = 1, ParameterSetName = "B")]
-        [System.Object] $Content,
-        [Parameter(Position = 2, ParameterSetName = "A")]
-        [Parameter(Position = 2, ParameterSetName = "C")]
+        [Parameter(Position = 1, ParameterSetName = "C")]
         [int] $Encoding,
-        [Parameter(Position = 2, ParameterSetName = "B")]
-        [Parameter(Position = 2, ParameterSetName = "D")]
+        [Parameter(Position = 1, ParameterSetName = "D")]
         [switch] $DefaultEncoding,
         
         [Parameter(ParameterSetName = "")]
         [switch] $NoNewline,
-        [Parameter(ValueFromPipeline, ParameterSetName = "C")]
-        [Parameter(ValueFromPipeline, ParameterSetName = "D")]
+        [Parameter(ParameterSetName = "")]
+        [switch] $Append,
+        [Parameter(ValueFromPipeline, ParameterSetName = "")]
         [System.Object] $InputObject
     )
-    # 從管道輸入
-    if ($InputObject) { $Content = $InputObject }
-    # 獲取編碼
-    if ((!$Encoding) ) {
-        $Enc = New-Object System.Text.UTF8Encoding $False
-        # $Enc = [Text.Encoding]::Default
-    } elseif ($Encoding -eq 65001) {
-        $Enc = New-Object System.Text.UTF8Encoding $False
-    } else {
-        $Enc = [Text.Encoding]::GetEncoding($Encoding)
-    } 
-    # 建立檔案
-    if ( Test-Path $Path -PathType:Leaf ) {
-        (New-Item $Path -ItemType:File -Force)|Out-Null
+    BEGIN {
+        $Idx = 0
+        $Count = 0
+        # 從管道輸入
+        if ($InputObject) {
+            Write-Host "intput==="$InputObject
+            $Content = $InputObject
+        }
+        
+        # 獲取編碼
+        if ($DefaultEncoding) {
+            # 使用當前系統編碼
+            $Enc = [Text.Encoding]::Default
+        }
+        elseif ((!$Encoding) ) {
+            # 完全不指定預設預設URF8
+            $Enc = New-Object System.Text.UTF8Encoding $False
+        }
+        elseif ($Encoding -eq 65001) {
+            # 指定UTF8
+            $Enc = New-Object System.Text.UTF8Encoding $False
+        }
+        else {
+            # 使用者指定
+            $Enc = [Text.Encoding]::GetEncoding($Encoding)
+        }
+        
+        # 建立檔案
+        if (!$Append) {
+            (New-Item $Path -ItemType:File -Force) | Out-Null
+        }
+    } process{
+        # 寫入檔案
+        if($Idx -eq 0){
+            [System.IO.File]::AppendAllText($Path, "$_", $Enc);
+        } else {
+            [System.IO.File]::AppendAllText($Path, "`n$_", $Enc);
+        } $Idx++
     }
-    # 寫入檔案
-    if ($NoNewline) {
-        [System.IO.File]::WriteAllLines($Path, ($Content[0..($Content.Count-2)]), $Enc);
-        [System.IO.File]::AppendAllText($Path, ($Content[-1]), $Enc);
-    } else {
-        [System.IO.File]::WriteAllLines($Path, $Content, $Enc);
+    END {
+        # [System.IO.File]::AppendAllText($Path, "`n", $Enc);
+        Write-Host "最後一行==[$Idx][$_]"
+        if ($_ -eq "") {
+        }
     }
 }
-$Content = "漢字"
-# $Content|WriteContent "Out.txt" -Encoding:65001
-$Content|WriteContent "Out.txt" 65001
+# $Content = "漢字"
+# $Content|WriteContent "Out.txt" -DefaultEncoding
+# $Content|WriteContent "Out.txt" 65001
+# $Content|WriteContent "Out.txt"
+
+# (ReadContent "Encoding_SHIFT.txt" 932)|WriteContent "Out.txt" 65001
+
+# 各種編碼讀寫範例
+# (ReadContent "Encoding_UTF8.txt")|WriteContent "Out_BIG5.txt" 950
+# (ReadContent "0.txt")|WriteContent "Out_BIG5.txt" 950
+# (ReadContent "2.txt").Count
+# (ReadContent "1.txt")|WriteContent "Out_BIG5.txt" 950
+
+# $Enc1 = [Text.Encoding]::GetEncoding(65001)
+# $Enc2 = New-Object System.Text.UTF8Encoding $False
+# (Get-Content "1.txt" -Encoding:UTF8)
+
+$str = [System.IO.File]::ReadLines("0.txt")
+# $str
+
+$str.Length
+
+# (ReadContent "2.txt")|WriteContent "Out_BIG5.txt" 950
+# (ReadContent "Encoding_BIG5.txt" 950)|WriteContent "Out_BIG5.txt" 950
+# (ReadContent "Encoding_UTF8.txt")|WriteContent "Out_BIG5.txt" 950
+
+# function cvEncode{
+#     param (
+#         $scrPath,
+#         $dstPath
+#     )
+#     $Content = Get-Content $scrPath -Encoding:Default
+#     $Encode = New-Object System.Text.UTF8Encoding $False
+#     New-Item -ItemType File -Path $dstPath -Force | Out-Null
+#     [System.IO.File]::WriteAllLines($dstPath, $Content, $Encode)
+# }
+# cvEncodeToUTF8 Encode_Default.txt Encode_UTF8.txt
